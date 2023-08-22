2eeec75c0eeda70b59718fb89b749f504f818992
潘磊 <2992930455@qq.com>
2023-08-22 18:22:08 +0800

    [main] first commit

---
+++ src/test/java/com/pl/socketioserver/SocketIoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.socketioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketIoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
